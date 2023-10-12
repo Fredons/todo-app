7af4026b5c5b8d1f7192f78189a7700040565e2a
Frederick <akamfrederick@gmail.com>
2023-10-12 16:13:58 +0100

    Ensure that the "Add" button cannot add an empty to-do

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ sg.theme("TanBlue")
 clock = sg.Text("", key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", tooltip="Add todo")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
@@ -29,11 +29,15 @@ while True:
 
     match event:
         case "Add":
-            todos = functions.get_todos()
-            new_todo = value['todo'] + '\n'
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            todo_text = value['todo'].strip()  # Remove leading and trailing whitespace
+            if todo_text:  # Check if the input is not empty
+                todos = functions.get_todos()
+                new_todo = value['todo'] + '\n'
+                todos.append(new_todo)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            else:
+                sg.popup("Cannot add an empty to-do. Please enter a task.", font=("Helvetica", 20))
         case sg.WINDOW_CLOSED:
             break
 
